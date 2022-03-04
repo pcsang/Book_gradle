dcca33b9d3acae59d3fecf754929bb8e26dd81cc
pcsang <pcsangb1706521@gmail.com>
2022-03-04 10:02:23 +0700

    commit file configuration swagger

--- src/main/java/com/example/demoBook/book/BookController.java
+++ src/main/java/com/example/demoBook/book/BookController.java
@@ -1,47 +1,53 @@
-//package com.example.demoBook.book;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@RestController
-//@RequestMapping(path = "api/v1/book")
-//public class BookController {
-//
-//    private final BookService bookService;
-//
-//    @Autowired
-//    public BookController(BookService bookService) {
-//        this.bookService = bookService;
-//    }
-//
-//    @GetMapping
-//    public List<Book> getBook(){
-//        return bookService.getBook();
-//    }
-//
-//    @GetMapping(path = "/{idBook}")
-//    public Optional<Book> getABook(@PathVariable("idBook") int idBook){
-//        return bookService.getABookId(idBook);
-//    }
-//
-//    @PostMapping
-//    public void registerNewBook(@RequestBody Book book){
-//        bookService.addNewBook(book);
-//    }
-//
-//    @DeleteMapping(path = "{idBook}")
-//    public void deleteBook(@PathVariable("idBook") int idBook){
-//        bookService.deleteBook(idBook);
-//    }
-//
-//    @PutMapping(path = "{idBook}")
-//    public void updateBook(@PathVariable("idBook") int idBook,
-//                           @RequestParam(required = false) String name,
-//                           @RequestParam(required = false) String author){
-//        bookService.updateBooks(idBook, name, author);
-//    }
-//}
-//
+package com.example.demoBook.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/book")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getBook(){
+        return bookService.getBook();
+    }
+
+    @GetMapping(path = "/{idBook}")
+    public Optional<Book> getABook(@PathVariable("idBook") int idBook){
+        return bookService.getABookId(idBook);
+    }
+
+    @GetMapping(path = "/test")
+    public List<Book> getBookAuthorAndCategory(@RequestParam(required = false) String author,
+                                               @RequestParam(required = false) String category){
+        return bookService.getBookAuthor_Category(author, category);
+    }
+
+    @PostMapping
+    public void registerNewBook(@RequestBody Book book){
+        bookService.addNewBook(book);
+    }
+
+    @DeleteMapping(path = "{idBook}")
+    public void deleteBook(@PathVariable("idBook") int idBook){
+        bookService.deleteBook(idBook);
+    }
+
+    @PutMapping(path = "/{idBook}")
+    public void updateBook(@PathVariable("idBook") int idBook,
+                           @RequestParam(required = false) String name,
+                           @RequestParam(required = false) String author){
+        bookService.updateBooks(idBook, name, author);
+    }
+}
+
--- src/main/java/com/example/demoBook/book/BookRepository.java
+++ src/main/java/com/example/demoBook/book/BookRepository.java
@@ -1,9 +1,14 @@
 package com.example.demoBook.book;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface BookRepository extends JpaRepository<Book, Integer> {
-//
-//}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query("SELECT b FROM Book b WHERE b.author LIKE ?1 AND b.category LIKE ?2")
+    List<Book> findBookAuthorAndCategory(String author, String category);
+}
