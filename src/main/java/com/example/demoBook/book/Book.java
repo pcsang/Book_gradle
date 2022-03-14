95efd0847a20dd12ec9a7fbdb93630ef84a60f25
PCSang <88499749+pcsang@users.noreply.github.com>
2022-03-14 23:05:23 +0700

    update file application.yml

--- src/main/java/com/example/demoBook/book/Book.java
+++ src/main/java/com/example/demoBook/book/Book.java
@@ -8,18 +8,7 @@ import java.time.LocalDate;
 @Entity
 @Table
 public class Book {
-
-
     @Id
-//    @SequenceGenerator(
-//            name = "book_sequence",
-//            sequenceName = "book_sequence",
-//            allocationSize = 1
-//    )
-//    @GeneratedValue(
-//            strategy =  GenerationType.SEQUENCE,
-//            generator = "book_sequence"
-//    )
     private int id;
     private String name;
     private String author;
