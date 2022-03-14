f79d2a4da54b91cb0c1442ec2a9cf25293478401
pcsang <pcsangb1706521@gmail.com>
2022-03-14 11:02:06 +0700

    update files testing controll - repository - service

--- src/main/java/com/example/demoBook/book/Book.java
+++ src/main/java/com/example/demoBook/book/Book.java
@@ -11,15 +11,15 @@ public class Book {
 
 
     @Id
-    @SequenceGenerator(
-            name = "book_sequence",
-            sequenceName = "book_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy =  GenerationType.SEQUENCE,
-            generator = "book_sequence"
-    )
+//    @SequenceGenerator(
+//            name = "book_sequence",
+//            sequenceName = "book_sequence",
+//            allocationSize = 1
+//    )
+//    @GeneratedValue(
+//            strategy =  GenerationType.SEQUENCE,
+//            generator = "book_sequence"
+//    )
     private int id;
     private String name;
     private String author;
