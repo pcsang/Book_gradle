04bb3a15b779cd09e6dd825072b85796682cb724
PCSang <88499749+pcsang@users.noreply.github.com>
2022-03-16 00:20:31 +0700

    update code checkstyle new

--- src/test/java/com/example/demoBook/DemoBookApplicationTests.java
+++ src/test/java/com/example/demoBook/DemoBookApplicationTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoBookApplicationTests {
-
-	@Test
-	void contextLoads() {}
+class DemoBookApplicationTests {@Test
+     void contextLoads() {}
 }
