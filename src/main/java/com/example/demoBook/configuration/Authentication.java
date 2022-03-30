f7b30f41ccf47f3de74f1ca24f84a9a776458901
PCSang <88499749+pcsang@users.noreply.github.com>
2022-03-30 23:26:50 +0700

    update authentication

--- src/main/java/com/example/demoBook/configuration/Authentication.java
+++ src/main/java/com/example/demoBook/configuration/Authentication.java
@@ -1,11 +1,17 @@
 package com.example.demoBook.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +28,9 @@ public class Authentication  extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
         auth.inMemoryAuthentication()
                 .withUser("sang")
                 .password("{noop}sangpass")
