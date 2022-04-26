26e5e8ae4497ad186fda6d833be81a5868880e1d
pcsang <pcsangb1706521@gmail.com>
2022-04-26 12:33:30 +0700

    Commit test

--- src/main/java/com/example/demoBook/service/BookService.java
+++ src/main/java/com/example/demoBook/service/BookService.java
@@ -14,61 +14,61 @@ import com.example.demoBook.repository.BookRepository;
 @Service
 public class BookService {
 
-    private final BookRepository bookRepository;
-
-    @Autowired
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
-
-    public List<Book> getBooks() {
-        return bookRepository.findAll();
-    }
-
-    public void addNewBook(Book book) {
-        bookRepository.save(book);
-    }
-
-
-    public void deleteBook(int id) {
-        boolean exists = bookRepository.existsById(id);
-        String messenger = "Book with id"+id+"does not exists";
-        if(!exists){
-            throw new IllegalStateException(messenger);
-        }
-        bookRepository.deleteById(id);
-    }
-
-    @Transactional
-    public Book updateBook(int idBook, String name, String author) {
-        boolean existBook = bookRepository.existsById(idBook);
-        if(!existBook){
-            throw new IllegalStateException("Book with id "+idBook+" does not exists");
-        }
-
-        Optional<Book> bookOption = bookRepository.findById(idBook);
-        Book book = bookOption.get();
-        if(name != null && name.length() > 0 && !Objects.equals(book.getName(), name)) {
-            book.setName(name);
-        }
-
-        if(author != null && author.length() > 0 && !Objects.equals(book.getAuthor(), author)) {
-            book.setAuthor(author);
-        }
-        return bookRepository.save(book);
-    }
-
-    public Optional<Book> getABookId(int idBook) {
-        boolean exists = bookRepository.existsById(idBook);
-        if(!exists) {
-            throw new IllegalStateException("Book with id "+idBook+" does not exist.");
-        }
-        return bookRepository.findById(idBook);
-    }
-
-    public List<Book> getBookAuthor_Category(String author, String category) {
-        return bookRepository.findBookAuthorAndCategory(author, category);
-    }
+//    private final BookRepository bookRepository;
+//
+//    @Autowired
+//    public BookService(BookRepository bookRepository) {
+//        this.bookRepository = bookRepository;
+//    }
+//
+//    public List<Book> getBooks() {
+//        return bookRepository.findAll();
+//    }
+//
+//    public void addNewBook(Book book) {
+//        bookRepository.save(book);
+//    }
+//
+//
+//    public void deleteBook(int id) {
+//        boolean exists = bookRepository.existsById(id);
+//        String messenger = "Book with id"+id+"does not exists";
+//        if(!exists){
+//            throw new IllegalStateException(messenger);
+//        }
+//        bookRepository.deleteById(id);
+//    }
+//
+//    @Transactional
+//    public Book updateBook(int idBook, String name, String author) {
+//        boolean existBook = bookRepository.existsById(idBook);
+//        if(!existBook){
+//            throw new IllegalStateException("Book with id "+idBook+" does not exists");
+//        }
+//
+//        Optional<Book> bookOption = bookRepository.findById(idBook);
+//        Book book = bookOption.get();
+//        if(name != null && name.length() > 0 && !Objects.equals(book.getName(), name)) {
+//            book.setName(name);
+//        }
+//
+//        if(author != null && author.length() > 0 && !Objects.equals(book.getAuthor(), author)) {
+//            book.setAuthor(author);
+//        }
+//        return bookRepository.save(book);
+//    }
+//
+//    public Optional<Book> getABookId(int idBook) {
+//        boolean exists = bookRepository.existsById(idBook);
+//        if(!exists) {
+//            throw new IllegalStateException("Book with id "+idBook+" does not exist.");
+//        }
+//        return bookRepository.findById(idBook);
+//    }
+//
+//    public List<Book> getBookAuthor_Category(String author, String category) {
+//        return bookRepository.findBookAuthorAndCategory(author, category);
+//    }
 }
 
 
--- src/test/java/com/example/demoBook/repositoryTest/BookRepositoryTest.java
+++ src/test/java/com/example/demoBook/repositoryTest/BookRepositoryTest.java
@@ -41,3 +41,46 @@
 //        assertThat(books).isNotEqualTo(book);
 //    }
 //}
+////package com.example.demoBook.repositoryTest;
+////
+////import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+////
+////import java.time.LocalDate;
+////import java.util.List;
+////
+////import org.junit.jupiter.api.Test;
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+////
+////import com.example.demoBook.book.Book;
+////import com.example.demoBook.repository.BookRepository;
+////
+////@DataJpaTest
+////class BookRepositoryTest {
+////
+////    @Autowired
+////    private BookRepository bookReposTest;
+////
+////    @Test
+////    void findBookAuthorAndCategory() {
+////        Book book = new Book(2, "10000 cau hoi vi sao?", "PhamSang",
+////                "Khoahoc", "Sach kham pha",
+////                LocalDate.of(2010, 04, 12),
+////                LocalDate.of(2015, 8,11));
+////
+////        List<Book> books = bookReposTest.findBookAuthorAndCategory("PhamSang", "Khoahoc");
+////        assertThat(books).asList();
+////    }
+////
+////    @Test
+////    void findBookAuthorAndCategoryNot() {
+////        Book book =  new Book(2, "The war", "phamsang",
+////                "Khoahoc", "quan su",
+////                LocalDate.of(2010,05,12), LocalDate.of(2015,05,15));
+////
+////        bookReposTest.save(book);
+////
+////        List<Book> books = bookReposTest.findBookAuthorAndCategory("phamsang", "sangABc");
+////        assertThat(books).isNotEqualTo(book);
+////    }
+////}
